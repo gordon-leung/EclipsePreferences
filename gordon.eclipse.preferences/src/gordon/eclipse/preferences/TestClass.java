95cd87e1f838ac5392b34b565ada74361ab89be6
Gordon <gordon@alacrityfoundation.com>
2013-06-27 17:08:57 -0700

    eclipse preferences

---
+++ gordon.eclipse.preferences/src/gordon/eclipse/preferences/TestClass.java
@@ -0,0 +1,15 @@
+package gordon.eclipse.preferences;
+
+public class TestClass {
+
+	private static final String ECLIPSE_PREFERENCES = "Eclipse Preferences!";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(ECLIPSE_PREFERENCES);
+	}
+
+}
